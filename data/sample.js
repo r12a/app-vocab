569b10ff249411abadeb3874f1994ee501734f80
r12a <ishida@w3.org>
2020-05-18 09:54:40 +0100

    sample, Order update

--- data/sample.js
+++ data/sample.js
@@ -1,17 +1,17 @@
 ﻿var wordList = new Array (
-"人|ʐən˧˥|person",
-"男人|nan˧˥ʐən˧˥|man",
-"女人|ny˨˩ʐən˧˥|woman",
-"孩子|xai˧˥tsɨ|child",
-"妻子|tɕʰi˥tsɨ|wife",
-"丈夫|tʂɑŋ˥˩fu|husband",
-"母亲|mu˨˩tɕʰin˥|mother",
-"父亲|fu˥˩tɕʰin˥|father",
-"动物|tʊŋ˥˩wu˥˩|animal",
-"鱼|y˧˥|fish",
-"鸟|niɑu˨˩˦|bird",
-"狗|kou˨˩˦ |dog",
-'✓|✓|✓'
+"人子|test",
+"人|person|ʐən˧˥",
+"男人|man|nan˧˥ʐən˧˥",
+"女人|woman|ny˨˩ʐən˧˥",
+"孩子|child|xai˧˥tsɨ",
+"妻子|wife|tɕʰi˥tsɨ",
+"丈夫|husband|tʂɑŋ˥˩fu",
+"母亲|mother|mu˨˩tɕʰin˥",
+"父亲|father|fu˥˩tɕʰin˥",
+"动物|animal|tʊŋ˥˩wu˥˩",
+"鱼|fish|y˧˥",
+"鸟|bird|niɑu˨˩˦",
+"狗|dog|kou˨˩˦ ",
 )
 
 
